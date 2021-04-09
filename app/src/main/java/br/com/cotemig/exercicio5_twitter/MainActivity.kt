1b04a024702cd4d3cfcb595a3d18a423de2db11d
JoaoCLAragao <71600841@aluno.faculdadecotemig.br>
2021-04-08 23:40:06 -0300

    Ultimo Commit

--- app/src/main/java/br/com/cotemig/exercicio5_twitter/MainActivity.kt
+++ app/src/main/java/br/com/cotemig/exercicio5_twitter/MainActivity.kt
@@ -54,6 +54,92 @@ class MainActivity : AppCompatActivity() {
                 0
             )
         )
+        feed.add(
+            Post(
+                R.drawable.tobias_profile_pic,
+                "Tobias van Schneider",
+                "@vanschneider",
+                "20m",
+                "My girlfriend is not even gone for more than 24h (traveling) and I managed to make the apartment look like this:",
+                R.drawable.tobias_tweet_image
+            )
+        )
+
+        feed.add(
+            Post(
+                R.drawable.oykun_profile_pic,
+                "Oykun",
+                "@oykun",
+                "1m",
+                "New addiction: Suits! #amazonprime",
+                0
+            )
+        )
+
+        feed.add(
+            Post(
+                R.drawable.azizfirat_profile_pic,
+                "Aziz Firat",
+                "@azizfirat",
+                "2m",
+                "Late night designing or playing Fifa?",
+                0
+            )
+        )
+
+        feed.add(
+            Post(
+                R.drawable.jonatan_profile_pic,
+                "Jonatan Castro",
+                "@jonatan",
+                "2m",
+                "Designer’s worst nightmare: designing its own portfolio.",
+                0
+            )
+        )
+        feed.add(
+            Post(
+                R.drawable.tobias_profile_pic,
+                "Tobias van Schneider",
+                "@vanschneider",
+                "20m",
+                "My girlfriend is not even gone for more than 24h (traveling) and I managed to make the apartment look like this:",
+                R.drawable.tobias_tweet_image
+            )
+        )
+
+        feed.add(
+            Post(
+                R.drawable.oykun_profile_pic,
+                "Oykun",
+                "@oykun",
+                "1m",
+                "New addiction: Suits! #amazonprime",
+                0
+            )
+        )
+
+        feed.add(
+            Post(
+                R.drawable.azizfirat_profile_pic,
+                "Aziz Firat",
+                "@azizfirat",
+                "2m",
+                "Late night designing or playing Fifa?",
+                0
+            )
+        )
+
+        feed.add(
+            Post(
+                R.drawable.jonatan_profile_pic,
+                "Jonatan Castro",
+                "@jonatan",
+                "2m",
+                "Designer’s worst nightmare: designing its own portfolio.",
+                0
+            )
+        )
 
         list_feed.adapter = adapter_list_feed(this, feed)
 
